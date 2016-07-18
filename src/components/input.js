8790f1f79f4b091e35cef076d850b13fc5526004
Nate Blain <blain89@gmail.com>
2016-07-17 18:15:58 -0700

    [chore] remove unnecessary function and change symantics of getRandomColor function

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import SVG from './svgElement';
+import SVG from './svg';
+import Circle from './circle';
 
 class Input extends Component {
   constructor(props) {
@@ -11,18 +12,24 @@ class Input extends Component {
     }
   }
 
-  // componentWillMount() {
-  //   this.state.circles = circles;
-  // }
+  _getRandomColor() {
+    var hexChar = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += hexChar[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
   _drawCircles(circleNum) {
     this.setState({ circleNum });
   console.log('here is the circleNum', this.state.circleNum);
     let circles = [];
     for (let i = 0; i < circleNum; i++) {
+      var color = this._getRandomColor();
       var cx = Math.floor(Math.random()*570);
       var cy = Math.floor(Math.random()*370);
-      circles.push(<circle key={i} cx={cx} cy={cy} r="30"></circle>)
+      circles.push(<Circle key={i} cx={cx} cy={cy} sty={color} r="30" />)
     }
     this.setState({ circles });
     console.log(this.state.circleNum, this.state.circles);
