e261306968f854d134012c78a358d8b196010f8b
Nate Blain <blain89@gmail.com>
2016-07-17 18:13:47 -0700

    [chore] remove spaces and add comments

--- src/components/circle.js
+++ src/components/circle.js
@@ -9,22 +9,23 @@ class Circle extends Component {
     this._dragmove = this._dragmove.bind(this);
   }
 
+  // attach drag functionality after mounting
   componentDidMount() {
     d3.select(this.refs.myCircle)
       .call(d3.drag().on('drag', this._dragmove));
   }
 
+  // define drag movement
   _dragmove() {
     d3.select(this.refs.myCircle)
+      .classed("grabbing", true)
       .attr("cx", d3.event.x)
       .attr("cy", d3.event.y);
   }
 
   render() {
     return (
-
-        <circle ref="myCircle" className="circle" cx={this.props.cx} cy={this.props.cy} r="30" style={{fill: this.props.sty}}></circle>
-
+      <circle ref="myCircle" className="circle" cx={this.props.cx} cy={this.props.cy} r="30" style={{fill: this.props.sty}}></circle>
     )
   }
 }
